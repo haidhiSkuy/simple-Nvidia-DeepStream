7febe9214a40749c34f6690587ab22fe28791e3a
Haidhi <haidhiangkawijana@gmail.com>
2024-04-26 03:59:20 +0000

    run via yaml config

--- widya-deepstream/run.py
+++ widya-deepstream/run.py
@@ -1,26 +1,29 @@
+import yaml
 import argparse
 from widya_pipeline import PipelineCommon
 
 parser = argparse.ArgumentParser(description="Widya Deepstream")
 parser.add_argument( # 1
-    "-i", "--input", help="Path to input streams", 
-    metavar="URIs", nargs="+",
-    default=[
-        "file:///opt/nvidia/deepstream/deepstream-6.4/samples/streams/sample_720p.mp4", 
-    ])
-parser.add_argument( # 2
-    "-c", "--configfile", metavar="config_location.txt",
-    default="/workspaces/tes2/widya-deepstream/configs/widya-config-yolox.txt", 
-    help="Choose the config-file to be used with specified pgie",
+    "-s", "--yaml-config", help="Path yaml config file", 
+    default="/workspaces/tes2/widya-deepstream/run.yaml"
 )
 args = parser.parse_args() 
 
-
 if __name__ == "__main__":
+    with open(args.yaml_config, 'r') as file:
+        yaml_data = yaml.safe_load(file)
+
     pipeline = PipelineCommon(
-        args.input, 
-        args.configfile, 
+        source_files=yaml_data['source_files'], 
+        pgie_config_file=yaml_data['pgie_config_path'], 
+        sgie1_path=yaml_data['sgie1_config_path'], 
+        sgie2_path=yaml_data['sgie2_config_path'],
+        tracker_path=yaml_data['tracker_config_path'], 
+        nvods_func_name=yaml_data['nvods'], 
+        redis_config=yaml_data['redis_config'],  
+        saved_frames_folder=yaml_data['save_frames_folder']
     ) 
+
     pipeline.build_pipeline()
     pipeline.linking_pipeline()
     pipeline.run()
